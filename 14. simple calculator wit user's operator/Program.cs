cb9152c1f173de86cfbe94dcc2350d4d7e235e7c
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-09 20:48:38 +0200

    Add project files.

---
+++ 14. simple calculator wit user's operator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.simple_calculator_wit_user_s_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a name ");
+            Double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" Enter an operator");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Enter second number");
+            Double num2 = Convert.ToDouble(Console.ReadLine());
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+                Console.WriteLine(num1 / num2);
+            {
+                Console.WriteLine("Not an operator");
+            }
+            Console.Read();
+
+
+        }
+    }
+}
+           
+            
